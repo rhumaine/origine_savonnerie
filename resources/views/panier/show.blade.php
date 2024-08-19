2380e45f5b9b5442306cbbce2d2f72383b2e017d
rhumaine <romain.demay56@hotmail.fr>
2024-08-19 22:48:43 +0200

    modal

--- resources/views/panier/show.blade.php
+++ resources/views/panier/show.blade.php
@@ -54,7 +54,7 @@
             <p class="total mt-2">{{ number_format($total, 2, ',', ' ') }} €</p>
 
             <!-- Bouton pour passer à la caisse -->
-            <a href="" class="btn mt-3">Payer maintenant</a>
+            <a href="#" class="btn mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Valider le panier</a>
 
             <!-- Bouton pour vider le panier -->
             <form action="{{ route('panier.vider') }}" method="POST" style="margin-top: 20px;">
@@ -66,5 +66,41 @@
     @else
         <p>Votre panier est vide.</p>
     @endif
+
+
+     <!-- Modal -->
+     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <!-- Modal Left -->
+                <div class="modal-section modal-left">
+                    <h5 class="text-center">Pas encore inscrit ?</h5>
+                    <div class="text-center mb-3">
+                        <a href="#" class="btn mt-3">Achat rapide</a>
+                    </div>
+                    <div class="text-center">
+                        <a href="/register" class="btn mt-3">S'inscrire</a>
+                    </div>
+                </div>
+                <!-- Modal Right -->
+                <div class="modal-section modal-right">
+                    <h5 class="text-center">Connexion</h5>
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Entrez votre email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Mot de passe</label>
+                            <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe" required>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Se connecter</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
