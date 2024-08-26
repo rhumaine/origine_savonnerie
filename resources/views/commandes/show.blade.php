5974661b5343278f8209c8b370b81004933c795e
rhumaine <romain.demay56@hotmail.fr>
2024-08-26 20:21:16 +0200

    détail commande

--- resources/views/commandes/show.blade.php
+++ resources/views/commandes/show.blade.php
@@ -4,6 +4,13 @@
 
         <div class="bg-white shadow-md rounded-lg mb-6 p-4">
             <h2 class="text-xl font-semibold">Informations de la commande</h2>
+            <p>Nom : {{ $commande->user->name }}</p>
+            <p>Prenom : {{ $commande->user->prenom }}</p>
+            <p>Télephone : {{ $commande->user->telephone }}</p>
+            <p>Adresse : {{ $commande->user->address }}</p>
+            <p>Code postal : {{ $commande->user->code_postal }}</p>
+            <p>Ville : {{ $commande->user->ville }}</p>
+            </br>
             <p>Date de commande : {{ $commande->date_commande->format('d/m/Y') }}</p>
             <p>Mode de paiement : {{ ucfirst($commande->mode_paiement) }}</p>
             @php
@@ -17,15 +24,26 @@
 
         <div class="bg-white shadow-md rounded-lg p-4">
             <h3 class="text-lg font-semibold">Produits commandés</h3>
-            <ul class="list-disc ml-6 mt-2">
-                @foreach($commande->produit as $produit)
-                    <li class="mb-2">
-                        <span class="font-bold">{{ $produit->nom }}</span> -
-                        {{ $produit->pivot->quantite }} x {{ number_format($produit->pivot->prix_unitaire, 2) }} € =
-                        {{ number_format($produit->pivot->quantite * $produit->pivot->prix_unitaire, 2) }} €
-                    </li>
-                @endforeach
-            </ul>
+                <table>
+                    <thead>
+                        <tr>
+                            <th style="width:200px">Nom du produit</th>
+                            <th style="width:100px">P.U</th>
+                            <th style="width:100px">Quantité</th>
+                            <th style="width:100px">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($commande->produit as $produit)
+                        <tr>
+                            <td>{{ $produit->nom }}</td>
+                            <td>{{ number_format($produit->pivot->prix_unitaire, 2, ',', ' ') }} €</td>
+                            <td>{{ $produit->pivot->quantite }}</td>
+                            <td>{{ number_format($produit->pivot->prix_unitaire * $produit->pivot->prix_unitaire, 2, ',', ' ') }} €</td>
+                        </tr> 
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </x-app-layout>
