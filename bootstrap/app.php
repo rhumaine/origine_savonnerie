ad11d5acd86e9952b4d8e74c0f6e85a1c0059246
rhumaine <romain.demay56@hotmail.fr>
2024-08-30 00:01:20 +0200

    vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
