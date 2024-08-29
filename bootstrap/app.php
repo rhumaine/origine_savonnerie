759652d30aa338f7668eb4c24dd52ea86bf94b07
rhumaine <romain.demay56@hotmail.fr>
2024-08-30 01:00:14 +0200

    modif

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
-        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
