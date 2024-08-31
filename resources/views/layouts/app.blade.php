018dfd46f9f7a2d029f8f6a6c4d7127447485f42
rhumaine <romain.demay56@hotmail.fr>
2024-08-31 02:20:48 +0200

    secure asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- Inclure Bootstrap Icons si nécessaire -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('build/assets/app-C3YUR_0D.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('build/assets/app-C3YUR_0D.css') }}">
 
         <!-- Styles -->
         <style>
