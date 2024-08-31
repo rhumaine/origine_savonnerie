7f05e63359e1ff5b7965a1e1d9d939fd49f4ce99
rhumaine <romain.demay56@hotmail.fr>
2024-08-31 18:55:00 +0200

    session php

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -41,7 +41,6 @@ class PanierController extends Controller
     public function show(Request $request)
     {
         $panier = Session::get('panier', []);
-        dd(Session::get('panier', []));
         $total = 0;
 
         // Calculer le total de la commande
