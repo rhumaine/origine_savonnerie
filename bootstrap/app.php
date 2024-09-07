4f1a11c7cc2d258c70d4503c719e384af238556a
rhumaine <romain.demay56@hotmail.fr>
2024-09-07 03:35:13 +0200

    test ajour panier #5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->alias([
-            'admin' => AdminMiddleware::class,
-            'csrf' => VerifyCsrfToken::class,
+            'admin' => AdminMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
